39674cb1eeb5bc3e3d1d4224749766bbb0f22ad3
hasnank <hasnurk@gmail.com>
2016-11-08 04:40:34 +0700

    Edited transmitter.cpp

--- src/transmitter.cpp
+++ src/transmitter.cpp
@@ -6,7 +6,34 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[] ){
+	
+	/* Create socket */
+	printf("Membuat socket untuk koneksi ke %s:%s ...\n", argv[1], argv[2]);
+	
+	/* Create child process */
+	
+	// child receieve xon/xoff signal
+		printf("XOFF diterima.\n");
+		
+		printf("Menunggu XON...\n");
+		
+		printf("XON diterima.\n");
+	
+	// parent send data
+	
+	int i=1;
+	char cc;
+	FILE myfile = fopen(argv[3], "r"];
+	while (!eof()){
+		fscanf(myfile, %c, &cc);
+		printf("Mengirim byte ke-%d: \'%c\' \n", i, cc);
+		i++;		
+	}
+	fclose(myfile);
+		
+
+	
 	
 	return 0;
 }
