f704c03ccea566d49407f620aa6b15fc524d4b87
hasnank <hasnurk@gmail.com>
2016-11-22 12:35:50 +0700

    added support.h

--- src/support.cpp
+++ src/support.cpp
@@ -1,3 +1,5 @@
+#include "support.h"
+
 unsigned reverse(unsigned x) {
    x = ((x & 0x55555555) <<  1) | ((x >>  1) & 0x55555555);
    x = ((x & 0x33333333) <<  2) | ((x >>  2) & 0x33333333);
---
+++ support.h
@@ -0,0 +1,3 @@
+unsigned reverse(unsigned x);
+
+unsigned int crc32a(unsigned char *message);
