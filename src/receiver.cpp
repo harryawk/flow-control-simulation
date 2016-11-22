84f2c91a9a32bcad56effde3433239661bd856b0
harryawk <harry.kambey@gmail.com>
2016-11-22 22:23:45 +0700

    menambah logik error control

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -2,6 +2,7 @@
  * File : T1_rx.cpp
  */
 #include "dcomm.h"
+#include "support.h"
 #include <stdlib.h>
 #include <stddef.h>
 #include <netinet/in.h>
@@ -22,6 +23,10 @@
 
 /* Define receive buffer size */
 #define RXQSIZE 16
+#define WINDOWSIZE 6
+
+int window[WINDOWSIZE];
+int msgno = 0;
 
 MESGB messages[RXQSIZE];
 
@@ -40,6 +45,7 @@ char kirimXON[10];
 /* Functions declaration */
 static Byte *rcvchar(int, QTYPE *);
 static Byte *q_get(QTYPE *);
+static MESGB *rcvframe(int sockfd, QTYPE *q);
 
 struct sockaddr_in serv_addr, cli_addr;
 socklen_t addrlen = sizeof(serv_addr), clilen = sizeof(cli_addr);
@@ -62,7 +68,8 @@ void *childProcess(void *threadid){
 
 // SERVER PROGRAM
 int main(int argc, char *argv[]){
-	Byte c;
+	// Byte c;
+	MESGB c;
 	// create socket
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -101,9 +108,11 @@ int main(int argc, char *argv[]){
 	}
 	/*** IF PARENT PROCESS ***/
 	while(true){
-		c = *(rcvchar(sockfd, rxq));
+		// c = *(rcvchar(sockfd, rxq));
+		c = *(rcvframe(sockfd, rxq));
+		msgno++;
 		// Quit on end of file
-		if(c == Endfile){
+		if(c.data[0] == Endfile){
 			exit(0);
 		}
 	}
@@ -117,12 +126,20 @@ static Byte *rcvchar(int sockfd, QTYPE *q){
 
 	Byte *cur;
 	char c[10];
+	// MESGB c;
 	int byte_recv = recvfrom(sockfd, c, 1, 0, (struct sockaddr*)&cli_addr, &clilen);
 	if(byte_recv < 0){ //error receiving character
 		printf("Error receiving: %d", byte_recv);
 		exit(-2);
 	}
+/////////////////////////////////////
+	// cek struktur frame
+	
+	// cek checksum
 
+	// succeed, sendto(transmitter, ACK);
+
+////////////////////////////////////
 	// succeed reading character
 	q->count++;
 	q->data[q->rear] = c[0];
@@ -177,6 +194,65 @@ static Byte *q_get(QTYPE *q){
 	return current;
 }
 
-static MESGB *rcvframe(int sockfd){
+static MESGB *rcvframe(int sockfd, QTYPE *q){
+	MESGB *cur;
+	MESGB c[10];
+	int byte_recv = recvfrom(sockfd, c, sizeof(MESGB), 0, (struct sockaddr*)&cli_addr, &clilen);
+	if(byte_recv < 0){ //error receiving character
+		printf("Error receiving: %d", byte_recv);
+		exit(-2);
+	}
+/////////////////////////////////////
 	
+	// cek struktur frame
+	
+	if (c[0].soh != SOH || c[0].stx != STX || c[0].etx != ETX || c[0].msgno != msgno) {
+		printf("kirim NAK\n");
+		exit(-3);
+	}
+	
+	// cek checksum
+	
+	unsigned char checksumarr[5];
+	checksumarr[0] = c[0].soh;
+	checksumarr[1] = c[0].stx;
+	checksumarr[2] = c[0].etx;
+	checksumarr[3] = c[0].msgno;
+	checksumarr[4] = c[0].data[0];
+	unsigned int checksum = crc32(&checksumarr);
+	if (checksum != c[0].checksum) {
+		printf("kirim NAK\n");
+		exit(-4);
+	}
+
+	// succeed, sendto(transmitter, ACK);
+	
+	int send_ack = sendto(sockfd, ACK, sizeof(ACK), 0, (struct sockaddr*)&cli_addr, &clilen);
+	if(send_ack < 0){ //error sending ACK character
+		printf("Error send ACK: %d", send_ack);
+		exit(-2);
+	}
+////////////////////////////////////
+	// succeed reading character
+////////////////////////////////////
+	q->count++;
+	q->data[q->rear] = c[0].data;
+	cur.data = &(q->data[q->rear]);
+	q->rear++;
+	if(q->rear == RXQSIZE) {
+		q->rear = 0;
+	}
+
+	printf("Menerima frame ke-%d.\n", ++byte_idx);
+
+	// receive buffer above minimum upperlimit
+	// sending XOFF
+	if(q->count >= 8 && !send_xoff){
+		send_xoff = true;
+		send_xon = false;
+		printf("Buffer > minimum upperlimit.\n");
+		printf("Mengirim XOFF\n");
+		sendto(sockfd, kirimXOFF, 1, 0, (struct sockaddr*)&cli_addr, clilen);
+	}
+	return cur;
 }
