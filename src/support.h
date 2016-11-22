2c7479d3bb63f13d9fd14101b8784d3c24aa897d
harryawk <harry.kambey@gmail.com>
2016-11-23 00:29:02 +0700

    memperbaiki hasil merge

--- src/support.h
+++ src/support.h
@@ -1,14 +1,9 @@
-#ifndef _DCOMM_H_
-#define _DCOMM_H_
-
+#ifndef SUPPORT_H
+#define SUPPORT_H
 unsigned reverse(unsigned x);
 
 unsigned int crc32a(unsigned char *message);
 
-#endif
-=======
-#ifndef SUPPORT_H
-#define SUPPORT_H
 /*
 unsigned reverse(unsigned);
 unsigned int crc32a(unsigned char*);
